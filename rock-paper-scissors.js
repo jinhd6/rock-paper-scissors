d9d129676288fe0acb61ac3c15ca6422f65be8b2
Jinman Hong <jinhd6@gmail.com>
2022-09-04 20:46:24 +0900

    Create playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -15,4 +15,34 @@ const getComputerChoice = function() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+const playRound = function(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    let roundResult = '';
+
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        roundResult = 'Draw. Both are Rocks';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        roundResult = 'You Lose! Paper beats Rock';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        roundResult = 'You Win! Rock beats Scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        roundResult = 'You Win! Paper beats Rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        roundResult = 'Draw. Both are Papers';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        roundResult = 'You Lose! Scissors beat Paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        roundResult = 'You Lose! Rock beats Scissors';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        roundResult = 'You Win! Scissors beat Paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        roundResult = 'Draw. Both are Scissors';
+    }
+
+    return roundResult;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
